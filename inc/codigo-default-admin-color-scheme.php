9bb74b1151de5efee454d1435aa5995d2b5c9c03
Pablo Rica <pabloricaaldeguer@gmail.com>
2023-05-02 12:36:14 +0200

    Adding CMS colors

--- inc/codigo-default-admin-color-scheme.php
+++ inc/codigo-default-admin-color-scheme.php
@@ -19,3 +19,37 @@ defined( 'ABSPATH' ) || exit;
 if ( ! class_exists( 'Default_Admin_Color_Scheme' ) ) {
 	exit;
 }
+
+/**
+ * Add additional colour schemes to the backend
+ *
+ * @see https://www.hongkiat.com/blog/wordpress-admin-color-scheme/
+ * @see https://developer.wordpress.org/reference/functions/wp_color_scheme_settings/
+ * 
+ * @return void
+ */
+function additional_admin_color_schemes() {
+	
+	wp_admin_css_color( 
+		'yellow',
+		__( 'Yellow', 'codigo' ),
+		get_stylesheet_directory_uri() . '/admin-colors/yellow.css',
+		array( 
+			'#ebaf08', 
+			'#dd823b', 
+			'#fbd564', 
+			'#aaa088', 
+		),
+		array(
+			'base'    => '#fff',
+			'focus'   => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+}
+
+add_action(
+	'admin_init', 
+	'additional_admin_color_schemes' 
+);
